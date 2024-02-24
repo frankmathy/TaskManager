21cb3db9673cef0589b45ed3563887cc76f60965
Frank Mathy <frankmathy@gmail.com>
2024-02-24 20:47:57 +0100

    Adding of groups

--- TaskManager/ContentView.swift
+++ TaskManager/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationSplitView {
-            SidebarView(userCreatedGroups: userCreatedGroups, selection: $selection)
+            SidebarView(userCreatedGroups: $userCreatedGroups, selection: $selection)
         } detail: {
             switch(selection) {
             case .all:
