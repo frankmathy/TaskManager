c723a91cd6cf1597826764463dfaea202a26e750
Frank Mathy <frankmathy@gmail.com>
2024-02-24 12:30:23 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Frank Mathy on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
